93fad29b766882b11eea0ed3f25a2d5dae3e5565
Mohammad Fatehi <94982051+Mohammad-Fatehi@users.noreply.github.com>
2022-03-02 21:41:07 +0330

    Exercise1-3
    The product of multiplication of numbers, the product of division of numbers, the product of addition of numbers

---
+++ Exercise1-3/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <windows.h>
+
+/*
+ * Mohammad Fatehi
+ * Exercise1-3
+ */
+
+int main() {
+    double sum ,hit ,division
+    ,num1hit,num2hit,num3hit
+    ,num1division,num2division
+    ,num1sum,num2sum,num3sum,num4sum,num5sum;
+    printf("Enter three numbers to multiply :\n");
+    printf("1 : ");
+    scanf(" %lf",&num1hit);
+    printf("2 : ");
+    scanf(" %lf",&num2hit);
+    printf("3 : ");
+    scanf(" %lf",&num3hit);
+    hit=num1hit*num2hit*num3hit;
+    printf("Enter two numbers to divide :\n");
+    printf("1 : ");
+    scanf(" %lf",&num1division);
+    printf("2 : ");
+    scanf(" %lf",&num2division);
+    division=num1division/num2division;
+    printf("Enter five numbers to sum :\n");
+    printf("1 : ");
+    scanf(" %lf",&num1sum);
+    printf("2 : ");
+    scanf(" %lf",&num2sum);
+    printf("3 : ");
+    scanf(" %lf",&num3sum);
+    printf("4 : ");
+    scanf(" %lf",&num4sum);
+    printf("5 : ");
+    scanf(" %lf",&num5sum);
+    sum=num1sum+num2sum+num3sum+num4sum+num5sum;
+    printf("**\n");
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    printf(" %f",hit);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    printf(" **\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("&&&\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    printf(" %f ",division);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("\n");
+    printf("$$$");
+    printf("\n");
+    Beep(500,500);
+    Beep(500,500);
+    Beep(500,500);
+    printf("#\t\t\t#\t\t\t#\t\t\t#\t\t\t#\n");
+    printf("\n\n");
+    printf(" %f",sum);
+    printf("\n\n");
+    printf("@\t\t\t@\t\t\t@"
+           "\t\t\t@\t\t\t@\n");
+
+
+
+    return 0;
+}
